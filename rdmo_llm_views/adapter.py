982137acee53f09158ca3dd6e6c20321e6b27796
Jochen Klar <mail@jochenklar.de>
2026-04-04 15:12:37 +0200

    Add logging

--- rdmo_llm_views/adapter.py
+++ rdmo_llm_views/adapter.py
@@ -1,17 +1,35 @@
+import json
+import logging
+
 from django.conf import settings
 
+from langchain_core.messages import message_to_dict
+
 from rdmo.core.utils import markdown2html
+
+logger = logging.getLogger(__name__)
+logger_usage = logging.getLogger('rdmo_llm_views.usage')
 
 
 class LangChainAdapter:
     def on_render(self, prompt, model):
         args = {**settings.LLM_VIEWS_LLM_ARGS, **({'model': model} if model is not None else {})}
 
+        logger.debug('model="%s" prompt="%s"', args.get('model'), prompt)
+
         llm = self.get_llm(args)
 
         try:
             result = llm.invoke(prompt)
+
+            result_dict = message_to_dict(result)
+            logger.debug('result = %s', json.dumps(result_dict, indent=2))
+
+            usage_metadata = result_dict.get('data', {}).get('usage_metadata', {})
+            logger_usage.info(json.dumps({'model': args.get('model'), **usage_metadata}))
+
         except Exception as e:
+            logger.exception(e)
             return str(e)
 
         return markdown2html(result.content)
